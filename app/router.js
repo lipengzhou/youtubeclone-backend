ff59b410baaf9e411fc19a4198f474eb064800e2
lipengzhou <lpzmail@163.com>
2021-01-28 16:46:21 +0800

    获取用户订阅列表

--- app/router.js
+++ app/router.js
@@ -13,4 +13,5 @@ module.exports = app => {
   // 用户订阅
   router.post('/users/:userId/subscribe', auth, controller.user.subscribe)
   router.delete('/users/:userId/subscribe', auth, controller.user.unsubscribe)
+  router.get('/users/:userId/subscriptions', controller.user.getSubscriptions)
 }
